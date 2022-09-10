8ad863bcd8bd0688ce0c4ec7755ae10a67a9c337
trombiano1 <mfujitadev@gmail.com>
2022-09-10 15:18:56 +0900

    webpack CORS

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,13 @@ module.exports = {
     static: path.resolve(__dirname, 'dist'),
     port: process.env.PORT || 8080,
     hot: true,
-    allowedHosts: "all"
+    allowedHosts: "all",
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+
   },
   module: {
     rules: [
