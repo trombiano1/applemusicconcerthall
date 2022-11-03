d791dded85d1025f5724bf48de143e64cf31f924
trombiano1 <mfujitadev@gmail.com>
2022-11-03 20:36:31 +0900

    test: multilang

---
+++ locales/ja.js
@@ -0,0 +1,6 @@
+export default {
+    RANKED_HIGH_CONDITION: "全ブロガーの中で頑張ってブログを更新しているユーザーを上位表示します",
+    UNCATEGORIZED_RANKED_HIGH_CONDITION: "未分類のブロガーの中で頑張ってブログを更新しているユーザーを上位表示します",
+    SEE_ALL_BLOGGERS: "全ブロガーをもっとみる",
+    ...
+  }
