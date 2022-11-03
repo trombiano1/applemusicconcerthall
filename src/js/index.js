649674ee8a6a987685afeac57043cc2a13c2ac09
trombiano1 <mfujitadev@gmail.com>
2022-11-03 23:47:09 +0900

    fixed

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-import '../scss/styles.scss'
-import 'datatables.net-bs5'
-
-
-console.log(__("Hello World")); 
-require('./fetch.js');
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import '../scss/styles.scss'
+import 'datatables.net-bs5'
+
+// const __ = require('multi-lang')();
+
+require('./fetch.js');
